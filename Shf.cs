607d333fe5af846ffa377299d366c62c8990b917
LitQoo <litqoo@gmail.com>
2018-11-23 14:43:40 +0900

    셔플기능 추가

---
+++ Shf.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+public class Shf
+{
+    static private System.Random rng = new System.Random();  
+
+    static public void Shuffle(IList<int> list)  
+    {  
+        int n = list.Count;  
+        while (n > 1) {  
+            n--;  
+            int k = rng.Next(n + 1);  
+            int value = list[k];  
+            list[k] = list[n];  
+            list[n] = value;  
+        }  
+    } 
+}
